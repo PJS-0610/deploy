90376beda51398088bdad5e62284433555820472
PJS-0610 <ranige0610@naver.com>
2025-08-21 20:21:26 +0900

    fix trigger

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -437,6 +437,14 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
+    location /notifications/ {
+        proxy_pass http://127.0.0.1:${BACKEND_PORT}/notifications/;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
     # Frontend - React app (default fallback)
     location / {
         proxy_pass http://127.0.0.1:${PORT}/;
